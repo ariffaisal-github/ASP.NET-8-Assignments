dc452516186ee99b2bc656e2d1ac9e768d78de79
Arif Faisal <ariffaisal18.19@gmail.com>
2025-09-14 00:09:40 +0600

    init: library management setup

---
+++ Practice-Projects/LibraryManagement/Controllers/BookController.cs
@@ -0,0 +1,24 @@
+﻿using LibraryManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagement.Controllers {
+    public class BookController : Controller {
+        private List<BookViewModel> _books;
+        public BookController() {
+            _books = new List<BookViewModel>();
+            CreateDummyBooks();
+        }
+        public IActionResult GetAll() {
+            return View(_books);
+        }
+
+        private void CreateDummyBooks() {
+            _books.Add(new BookViewModel { Id = 1, Title = "The Great Gatsby", Author = "F. Scott Fitzgerald", Genre = "Fiction" });
+            _books.Add(new BookViewModel { Id = 2, Title = "To Kill a Mockingbird", Author = "Harper Lee", Genre = "Fiction" });
+            _books.Add(new BookViewModel { Id = 3, Title = "1984", Author = "George Orwell", Genre = "Dystopian" });
+            _books.Add(new BookViewModel { Id = 4, Title = "Pride and Prejudice", Author = "Jane Austen", Genre = "Romance" });
+            _books.Add(new BookViewModel { Id = 5, Title = "The Catcher in the Rye", Author = "J.D. Salinger", Genre = "Fiction" });
+            _books.Add(new BookViewModel { Id = 6, Title = "The Hobbit", Author = "J.R.R. Tolkien", Genre = "Fantasy" });
+        }
+    }
+}
---
+++ Practice-Projects/LibraryManagement/Models/BookViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace LibraryManagement.Models {
+    public class BookViewModel {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Author { get; set; }
+        public string? Genre { get; set; }
+    }
+}
